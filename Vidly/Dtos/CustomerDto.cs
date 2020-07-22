1a8dcabba529a57ae7828776f11afe4765bd6d0d
atfranc2 <52332881+atfranc2@users.noreply.github.com>
2020-07-22 16:31:32 -0400

    Create movie and customer APIs

---
+++ Vidly/Dtos/CustomerDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace Vidly.Dtos
+{
+    public class CustomerDto
+    {
+        [Required]
+        [StringLength(255)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string LastName { get; set; }
+
+        public Nullable<DateTime> BirthDate { get; set; }
+
+        public int Id { get; set; }
+
+        public bool isSubscribedToNewsLetter { get; set; }
+
+        public byte MembershipTypeId { get; set; }
+    }
+}
